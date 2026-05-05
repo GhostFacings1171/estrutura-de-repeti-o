69a90f1ef9eeec8c49d8d653ed943ea5fe8686fc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-04 21:59:46 -0300

    first commit

---
+++ src/wihle.java
@@ -0,0 +1,9 @@
+public class wihle {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <=1000000000) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
